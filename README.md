4c5b5ae8c08ebb8d4a1e3201ffdf43cc937d97c9
Bui Thanh Son <sonbuithanh306@gmail.com>
2024-05-30 21:02:57 +0700

    Switch features

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+- sudo npm install -g firebase-tools
+- flutter pub add firebase_core
+- flutter pub global activate flutterfire_cli
+- flutter pub global run flutterfire_cli path
--- lib/main_screen/start_screen.dart
+++ lib/main_screen/start_screen.dart
@@ -7,8 +7,37 @@ class StartScreen extends StatefulWidget {
   _StartScreenState createState() => _StartScreenState();
 }
 
-class _StartScreenState extends State<StartScreen> {
+class _StartScreenState extends State<StartScreen>
+    with SingleTickerProviderStateMixin {
   String textToShow = 'Hello'; // Biến lưu trữ văn bản của Text
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 200), // Duration of the animation
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 1.0, end: 1.2).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _onTap() {
+    setState(() {
+      // Cập nhật văn bản khi nút được nhấn
+      textToShow = 'Start Chatting With Our Chatbot to get to your questions !';
+    });
+    _controller.forward().then((value) => _controller.reverse());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +49,7 @@ class _StartScreenState extends State<StartScreen> {
           Positioned.fill(
             bottom: -650,
             child: Image.asset(
-              'assets/background_under.png', // Đường dẫn của hình nền
+              'assets/images/background_under.png', // Đường dẫn của hình nền
               fit: BoxFit.fitWidth,
             ),
           ),
@@ -32,7 +61,7 @@ class _StartScreenState extends State<StartScreen> {
                   CrossAxisAlignment.center, // Căn giữa theo trục chéo
               children: [
                 // Character image
-                Image.asset('assets/bot_start1.png', height: 300),
+                Image.asset('assets/images/bot_start1.png', height: 300),
                 const SizedBox(height: 250),
                 // Text
                 Text(
@@ -45,19 +74,16 @@ class _StartScreenState extends State<StartScreen> {
                   textAlign: TextAlign.center,
                 ),
                 const SizedBox(height: 0),
-                // Button
+                // Button with animation
                 GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      // Cập nhật văn bản khi nút được nhấn
-                      textToShow =
-                          'Start Chatting With Our Chatbot to get to your questions !';
-                    });
-                  },
-                  child: Image.asset(
-                    'assets/button_next.png', // Đường dẫn của hình ảnh nút
-                    height: 50,
-                    width: 120,
+                  onTap: _onTap,
+                  child: ScaleTransition(
+                    scale: _animation,
+                    child: Image.asset(
+                      'assets/images/button_next.png',
+                      height: 50,
+                      width: 120,
+                    ),
                   ),
                 ),
               ],
